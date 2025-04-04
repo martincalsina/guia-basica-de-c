f3a117d169fb994ee8965ce9c56a3a0b37cc3f69
martincalsina <martincalsina05@gmail.com>
2025-04-04 13:49:49 -0300

    estoy feliz

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC = gcc
 CFLAGS = -Wall -Wextra -pedantic
-TARGET = funcionrara
+TARGET = funcionrara2
 
 all: $(TARGET)
 
---
+++ funcionrara2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define FELIZ 0
+#define TRISTE 1
+
+int estado = TRISTE; // static duration. File scope
+
+void ser_feliz();
+void print_estado();
+
+int main(){
+    print_estado();
+    ser_feliz();
+    print_estado(); // qué imprime?
+}
+
+void ser_feliz(){
+    estado = FELIZ;
+}
+
+void print_estado(){
+    printf("Estoy %s\n", estado == FELIZ ? "feliz" : "triste");
+}
+
+//ahora sí ser_feliz modifica a la variable global de estado
